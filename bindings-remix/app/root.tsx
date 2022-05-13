9a9e8bb03b385261aace7c50d5370024d1cb9294
chimame <rito.tamata@gmail.com>
2022-05-13 16:12:50 +0900

    Modify response time slow to 5 sec

--- bindings-remix/app/root.tsx
+++ bindings-remix/app/root.tsx
@@ -8,6 +8,13 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+const wait = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+export async function loader() {
+  await wait(5000);
+  return {};
+}
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
