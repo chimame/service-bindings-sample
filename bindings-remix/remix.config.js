e45b25ca57904b4dfa0be9a611378061b0f38d20
chimame <rito.tamata@gmail.com>
2022-05-27 01:50:03 +0900

    Add remix-service-bindings package for Remix split deploy

--- bindings-remix/remix.config.js
+++ bindings-remix/remix.config.js
@@ -1,3 +1,24 @@
+const { withEsbuildOverride } = require("remix-esbuild-override");
+const remixServiceBindings = require("remix-service-bindings").default;
+
+withEsbuildOverride((option, { isServer }) => {
+  if (isServer) {
+    option.plugins = [
+      /**
+       * remixServiceBindings
+       * @param isEdgeSide {boolean} - When this is true, the build is for edge (binder) and when false, the build is for bindee.
+       *                               (Deployment (build) must be done in two parts.)
+       * @param bindingsName {string} - The bind name set in toml. This name will be converted to a bind object.
+       * @param enabled {boolean} - If this is false, this plugin is disabled.
+       */
+      remixServiceBindings(!process.env.BINDEE, "BINDEE", !!process.env.DEPLOY),
+      ...option.plugins,
+    ];
+  }
+
+  return option;
+});
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
--- bindings-remix/server.js
+++ bindings-remix/server.js
@@ -3,5 +3,11 @@ import * as build from "@remix-run/dev/server-build";
 
 addEventListener(
   "fetch",
-  createEventHandler({ build, mode: process.env.NODE_ENV })
+  createEventHandler({
+    build,
+    mode: process.env.NODE_ENV,
+    getLoadContext: (event) => {
+      return { event };
+    },
+  })
 );
